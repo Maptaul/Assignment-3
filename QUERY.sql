6ab8c48a779cb1f60c86ef9ab60fd32753eeb19d
Maptaul <maptaulislam1@gmail.com>
2026-06-15 00:28:32 +0600

    add Matches table with necessary fields and constraints

--- QUERY.sql
+++ QUERY.sql
@@ -5,3 +5,20 @@ CREATE TABLE Users (
   role VARCHAR(20) NOT NULL CHECK (role IN ('Ticket Manager', 'Football Fan')),
   phone_number VARCHAR(20)
 );
+
+
+
+CREATE TABLE Matches (
+  match_id INT PRIMARY KEY,
+  fixture VARCHAR(150) NOT NULL,
+  tournament_category VARCHAR(100) NOT NULL,
+  base_ticket_price NUMERIC(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
+  match_status VARCHAR(20) NOT NULL CHECK (
+    match_status IN (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
+);
